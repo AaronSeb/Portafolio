d47904500883718346355f18d5debdd6d3a8d0c0
Aaron Jimenez <aron_7jimenez@hotmail.com>
2023-07-04 15:15:37 -0500

    comentarios de validación de formulario

--- js/apps.js
+++ js/apps.js
@@ -2,9 +2,9 @@ import { valida } from "./validaciones.js";
 
 console.log("archivo js")
 
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('input');   //selecionar todos los inputs, el querySelectorAll, guarda a todos en arreglos
 inputs.forEach(input => {
-    input.addEventListener('blur',(input)=>{
-        valida(input.target);
+    input.addEventListener('blur',(input)=>{        //recorremos los inputs y por cada input dispara el evento 'blur', mediante array function le pasa el parametro input, 
+        valida(input.target);                       //y lo pasa a un callback valida, que recibe como parametro ese input.target, para saber específicamente cual
     })
 });
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,19 +1,19 @@
 //quiero saber el input seleccionado, para seleccionar el padre
-export function valida (input){
-    const tipoDeInput = input.dataset.tipo
-    console.log(input.parentElement)
+export function valida (input){                                     //exportamos a apps
+    const tipoDeInput = input.dataset.tipo                          //mediante la lista dataset selecciona el tipo
+    console.log(input.parentElement)                                //imprime el padre contenedor div que va ser modificado
 
     if(input.validity.valid){
-        input.parentElement.classList.remove("input-container--invalid")
-        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+        input.parentElement.classList.remove("input-container--invalid")            //valida para quitar la clase al padre
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";   //valida para limpiar
     }else {
-        input.parentElement.classList.add("input-container--invalid")
-        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput,input);
+        input.parentElement.classList.add("input-container--invalid")               //agrega la clase al padre
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput,input);     //imprime el mensaje de error, por tipo
     }
 }
 
 
-//quiero saber el tipo de error
+//quiero saber el tipo de error, creo el arreglo
 const tipoDeError =[
     "valueMissing",
     "typeMismatch",
@@ -48,14 +48,14 @@ const mensajesDeErrores = {
 
 //para imprimir el tipo de mensaje de error
 
-function mostrarMensajeDeError(tipoDeInput, input){
+function mostrarMensajeDeError(tipoDeInput, input){                     //le paso parametro tipoDeInput, input
     let mensaje = ''
     tipoDeError.forEach(error=>{
         if(input.validity[error]){
-            console.log(tipoDeInput,error)
+            console.log(tipoDeInput,error)      
             console.log(input.validity[error])
-            console.log(mensajesDeErrores[tipoDeInput][error])
-            mensaje = mensajesDeErrores[tipoDeInput][error]
+            console.log(mensajesDeErrores[tipoDeInput][error])      
+            mensaje = mensajesDeErrores[tipoDeInput][error]         //en la variable mensaje, le pongo los mensahes de errores y accedo a los objetos por [tipoDeInput] y [error]
         }
     })
     return mensaje;
